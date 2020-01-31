5a9c6b9db80c5d961f3084fa52f578f7b3865652
manuelatrotta <manuelat88@hotmail.it>
2020-01-31 17:13:11 +0100

    fine js // inizio miglioramento html e css

--- js/script.js
+++ js/script.js
@@ -21,9 +21,13 @@ $(document).ready(function() {
       for(var i = 0; i < cds.length; i++) {
         var cd = cds[i];
         console.log(cd);
-      }
+        var source = $('#entry-template').html();
+        var template = Handlebars.compile(source);
+        var html = template(cd);
 
-}
+        $('.cds-container').append(html);
+      }
+    }
 
 
 
