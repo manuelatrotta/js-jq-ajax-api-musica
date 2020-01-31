e2433cb8e21988b99e977f1c31f5d85cbdcdcb44
manuelatrotta <manuelat88@hotmail.it>
2020-01-31 16:21:34 +0100

    inizio ajax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
   alert('ehi oh let\' go');
+
+
+  $.ajax(
+    {
+      'url':'https://flynn.boolean.careers/exercises/api/array/music',
+      'method': 'GET',
+      'success': function (data) {
+        console.log(data);
+      },
+      'error': function (richiesta, stato, errori) {
+        alert('errore' + errors);
+      }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
 });
